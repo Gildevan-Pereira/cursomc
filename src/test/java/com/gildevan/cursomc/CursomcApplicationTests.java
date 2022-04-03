dae65b17e2ea483a84beca550dcba46fccad3a58
Gildevan Pereira <gildevanpereira.pro@gmail.com>
2022-04-03 16:41:51 -0300

    Projeto criado

---
+++ src/test/java/com/gildevan/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gildevan.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
